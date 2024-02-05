950dc902d7b23b3f0f4220ce4e34a6e269c20dbe
„Cvitan <cvitanking7@gmail.com>
2024-02-05 14:44:10 +0100

    izmjenjeno

--- Zavrsni_rad2/Core/Inc/lcd.h
+++ Zavrsni_rad2/Core/Inc/lcd.h
@@ -1,13 +1,13 @@
 #include "stm32f4xx_hal.h"
 
-void lcd_inicijalizacija (void);   // initialize lcd
+void lcd_inicijalizacija (void);   // inicijalizacija
 
 void lcd_slanje_naredbe (char naredba);  // slanje naredbe na lcd
 
-void lcd_slanje_podatka (char podatak);  // send data to the lcd
+void lcd_slanje_podatka (char podatak);  // slanje podatka na lcd
 
-void lcd_slanje_stringa (char *string);  // send string to the lcd
+void lcd_slanje_stringa (char *string);  // slanje stringa na lcd
 
-void lcd_postavi_kursor(int redak, int stupac);  // put cursor at the entered position row (0 or 1), col (0-15);
+void lcd_postavi_kursor(int redak, int stupac);  // postavljanje kursora
 
-void lcd_ocisti (void);
+void lcd_ocisti (void); //ciscenje
